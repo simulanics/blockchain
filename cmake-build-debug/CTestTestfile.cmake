68fdde96e6f4dd294bce9462384018dc06085f10
Yusuf Yildiz <yusuf.yildiz@kobil.com>
2018-06-07 16:24:21 +0300

    gitignore updated

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yusufyildiz/Documents/github/monetaverde/blockchain
-# Build directory: /Users/yusufyildiz/Documents/github/monetaverde/blockchain/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external")
-subdirs("src")
-subdirs("tests")
--- cmake-build-debug/external/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yusufyildiz/Documents/github/monetaverde/blockchain/external/gtest
-# Build directory: /Users/yusufyildiz/Documents/github/monetaverde/blockchain/cmake-build-debug/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
