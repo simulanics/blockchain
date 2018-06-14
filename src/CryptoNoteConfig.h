07b43ded8af6e8733378bd35dca136a0cdcf57fe
theyusufyildiz <yusufyildiz86@gmail.com>
2018-06-14 17:40:15 +0300

    TX Hex Value added.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -75,7 +75,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "monetaverde";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001bf93eff008029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121014d23a9e408e6ae671669fd67c731aa3089913a347ba563ee67140a8db397e0a1";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
